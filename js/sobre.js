88b9c4e89cbfea5417319c09d19bf5e386a1ede1
Leonardo Cunha <leonardo.cunha5@fatec.sp.gov.br>
2024-04-17 17:47:26 -0300

    Delete sobre.js

--- js/sobre.js
+++
@@ -1,9 +0,0 @@
-// var map = L.map('map').setView([-20.543594, -47.397108], 13);
-
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// L.marker([-20.543594, -47.397108]).addTo(map)
-//     .bindPopup('Venha já conhecer!<br> Estamos esperando por você.')
-//     .openPopup();
